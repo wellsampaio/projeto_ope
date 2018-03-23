f945ae81c64e42e24c7d09c8f55ce65327931c12
Wellington Sampaio <letovictalino@gmail.com>
2018-03-23 02:41:35 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -7,6 +7,8 @@ use \Slim\Slim;
 use \Hcode\Page;
 
 use \Hcode\PageAdmin;
+
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -25,6 +27,7 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();
 
 	$page = new PageAdmin();
 
@@ -32,6 +35,35 @@ $app->get('/admin', function() {
     
 	
 
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl("login");
+});
+
+$app->post("/admin/login", function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
