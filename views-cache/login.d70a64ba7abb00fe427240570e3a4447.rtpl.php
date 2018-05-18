ea4e3c797dc84c3aa5f23ded9f9dee355f980128
Wellington Sampaio <letovictalino@gmail.com>
2018-05-17 23:39:16 -0300

    Alteração do admin

--- views-cache/login.d70a64ba7abb00fe427240570e3a4447.rtpl.php
+++ views-cache/login.d70a64ba7abb00fe427240570e3a4447.rtpl.php
@@ -17,6 +17,7 @@
   <!-- iCheck -->
   <link rel="stylesheet" href="/res/adminplugins/iCheck/square/blue.css">
 
+
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
