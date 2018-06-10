3f2a3beb73fcd6314e45011b86c946480986322b
Wellington Sampaio <letovictalino@gmail.com>
2018-06-09 21:38:13 -0300

    Crud Categoria

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ use \Hcode\PageAdmin;
 
 use \Hcode\Model\User;
 
+use \Hcode\Model\Category;
+
 $app = new Slim();
 
 $app->config('debug', true);
@@ -21,7 +23,7 @@ $app->get('/', function() {
 
 	$page->setTpl("index");
     
-	
+	 
 
 });
 
@@ -244,7 +246,98 @@ $app->post("/admin/forgot/reset", function(){
 
 });
 
+$app->get("/admin/categories", function(){
 
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+	
+	header('Location: /admin/categories');
+	exit;
+
+
+});
+
+
+$app->get("/admin/categories/:idcategory", function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+
+	]);
+	
+
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+	header('Location: /admin/categories');
+	exit;
+	
+
+
+});
 $app->run();
 
  ?>
