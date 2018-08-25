b630a2d71e0d9dee584a0a07a4ef396daa82a17c
Wellington Sampaio <letovictalino@gmail.com>
2018-08-25 00:25:53 -0300

    Minha Conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -9,6 +9,8 @@ class User extends Model {
 	const SECRET = "HcodePhp7_Secret";
 	const ERROR = "UserError";
 	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSuccess";
+
 
 	public static function getFromSession()
 	{
@@ -251,6 +253,26 @@ class User extends Model {
 		$_SESSION[User::ERROR] = NULL;
 	}
 
+	public static function setSuccess($msg)
+	{
+		$_SESSION[User::SUCCESS] = $msg;
+	}
+
+	public static function getSuccess()
+	{
+		$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+
+		User::clearSuccess();
+
+		return $msg;
+	}
+
+	public static function clearSuccess()
+	{
+		$_SESSION[User::SUCCESS] = NULL;
+	}
+
+
 	public static function setErrorRegister($msg)
 	{
 		$_SESSION[User::ERROR_REGISTER] = $msg;
@@ -285,5 +307,7 @@ class User extends Model {
 			'cost'=>12
 		]);
 	}
+
+
 }
  ?>
