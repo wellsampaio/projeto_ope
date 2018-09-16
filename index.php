f54aadf9ceacc17ea45eb1ebdace54080edeb036
unknown <you@example.com>
2018-09-16 02:10:51 -0300

    Ajustes na Rota Cart

--- index.php
+++ index.php
@@ -9,18 +9,13 @@ $app = new Slim();
 $app->config('debug', true);
 
 require_once("functions.php");
-
 require_once("site.php");
-
 require_once("admin.php");
-
 require_once("admin-users.php");
-
 require_once("admin-products.php");
-
 require_once("admin-categories.php");
-
 require_once("admin-orders.php");
+require_once("cart.php");
 
 
 $app->run();
