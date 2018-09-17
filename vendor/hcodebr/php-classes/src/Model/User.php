4bf51af23995dadfc344d29609b7b6f6dc2b66d3
unknown <you@example.com>
2018-09-16 21:12:18 -0300

    Integração Pagseguro HTML

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -49,49 +49,74 @@ class User extends Model {
 		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
 			":LOGIN"=>$login
 		)); 
+
 		if (count($results) === 0)
 		{
+
 			throw new \Exception("Usuário inexistente ou senha inválida.");
+
 		}
+
 		$data = $results[0];
+
 		if (password_verify($password, $data["despassword"]) === true)
 		{
+
 			$user = new User();
+
 			$data['desperson'] = utf8_encode($data['desperson']);
+
 			$user->setData($data);
+
 			$_SESSION[User::SESSION] = $user->getValues();
+
 			return $user;
+
 		} else {
+
 			throw new \Exception("Usuário inexistente ou senha inválida.");
+
 		}
 	}
 
 	public static function verifyLogin($inadmin = true)
 	{
+		
 		if (!User::checkLogin($inadmin)) {
+			
 			if ($inadmin) {
+				
 				header("Location: /admin/login");
+
 			} else {
+
 				header("Location: /login");
 			}
+
 			exit;
 		}
 	}
 
 	public static function logout()
 	{
+
 		$_SESSION[User::SESSION] = NULL;
+
 	}
 
 	public static function listAll()
 	{
 		$sql = new Sql();
+
 		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
 	}
 
 	public function save()
 	{
+		
 		$sql = new Sql();
+		
 		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
 			":desperson"=>utf8_decode($this->getdesperson()),
 			":deslogin"=>$this->getdeslogin(),
@@ -100,23 +125,31 @@ class User extends Model {
 			":nrphone"=>$this->getnrphone(),
 			":inadmin"=>$this->getinadmin()
 		));
+
 		$this->setData($results[0]);
 	}
 
 	public function get($iduser)
 	{
+
 		$sql = new Sql();
+
 		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
 			":iduser"=>$iduser
 		));
+
 		$data = $results[0];
+
 		$data['desperson'] = utf8_encode($data['desperson']);
+
 		$this->setData($data);
 	}
 
 	public function update()
 	{
+
 		$sql = new Sql();
+
 		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
 			":iduser"=>$this->getiduser(),
 			":desperson"=>utf8_decode($this->getdesperson()),
@@ -126,6 +159,7 @@ class User extends Model {
 			":nrphone"=>$this->getnrphone(),
 			":inadmin"=>$this->getinadmin()
 		));
+
 		$this->setData($results[0]);		
 	}
 
@@ -139,7 +173,9 @@ class User extends Model {
 
 	public static function getForgot($email, $inadmin = true)
 	{
+
 		$sql = new Sql();
+		
 		$results = $sql->select("
 			SELECT *
 			FROM tb_persons a
@@ -151,10 +187,13 @@ class User extends Model {
 
 		if (count($results) === 0)
 		{
+
 			throw new \Exception("Não foi possível recuperar a senha.");
 			
 		}
+
 		else
+
 		{
 			$data = $results[0];
 			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
@@ -170,18 +209,24 @@ class User extends Model {
 			else
 			{
 			 $dataRecovery = $results2[0];
+
              $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+
              $code = openssl_encrypt($dataRecovery['idrecovery'], 'aes-256-cbc', User::SECRET, 0, $iv);
+
              $result = base64_encode($iv.$code);
+
              if ($inadmin === true) {
                  $link = "http://www.casadedonabrasilina.com.br/admin/forgot/reset?code=$result";
              } else {
                  $link = "http://www.casadedonabrasilina.com.br/forgot/reset?code=$result";
              } 
+
              $mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Confeitaria Casa de Dona Brasilina", "forgot", array(
                  "name"=>$data['desperson'],
                  "link"=>$link
              )); 
+             
              $mailer->send();
              return $data;
 			}
