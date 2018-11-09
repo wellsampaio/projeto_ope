06439ced2f585ca4c01b433e3d933e9b4703873e
unknown <you@example.com>
2018-11-09 01:38:28 -0200

    Ajustes front-end

--- views-cache/profile-orders-detail.81ae729f0caba5e43d3ad0b9b7fe8afd.rtpl.php
+++ views-cache/profile-orders-detail.81ae729f0caba5e43d3ad0b9b7fe8afd.rtpl.php
@@ -1,4 +1,4 @@
-<style>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><style>
 @media print {
     .header-area,
     .site-branding-area,
@@ -24,11 +24,11 @@
     <div class="container">
         <div class="row">                
             <div class="col-md-3">
-                {include="profile-menu"}
+                <?php require $this->checkTemplate("profile-menu");?>
             </div>
             <div class="col-md-9">
                 
-                <h3 id="order_review_heading" style="margin-top:30px;">Detalhes do Pedido N°{$order.idorder}</h3>
+                <h3 id="order_review_heading" style="margin-top:30px;">Detalhes do Pedido N°<?php echo htmlspecialchars( $order["idorder"], ENT_COMPAT, 'UTF-8', FALSE ); ?></h3>
                 <div id="order_review" style="position: relative;">
                     <table class="shop_table">
                         <thead>
@@ -38,21 +38,21 @@
                             </tr>
                         </thead>
                         <tbody>
-                            {loop="$products"}
+                            <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
                             <tr class="cart_item">
                                 <td class="product-name">
-                                    {$value.desproduct} <strong class="product-quantity">× {$value.nrqtd}</strong> 
+                                    <?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?> <strong class="product-quantity">× <?php echo htmlspecialchars( $value1["nrqtd"], ENT_COMPAT, 'UTF-8', FALSE ); ?></strong> 
                                 </td>
                                 <td class="product-total">
-                                    <span class="amount">R$ {$value.vltotal}</span>
+                                    <span class="amount">R$ <?php echo htmlspecialchars( $value1["vltotal"], ENT_COMPAT, 'UTF-8', FALSE ); ?></span>
                                 </td>
                             </tr>
-                            {/loop}
+                            <?php } ?>
                         </tbody>
                         <tfoot>
                             <tr class="cart-subtotal">
                                 <th>Subtotal</th>
-                                <td><span class="amount">R$ {$cart.vlsubtotal}</span>
+                                <td><span class="amount">R$ <?php echo htmlspecialchars( $cart["vlsubtotal"], ENT_COMPAT, 'UTF-8', FALSE ); ?></span>
                                 </td>
                             </tr>
                             <tr class="shipping">
@@ -64,7 +64,7 @@
                             </tr>
                             <tr class="order-total">
                                 <th>Total do Pedido</th>
-                                <td><strong><span class="amount">R$ {$cart.vltotal}</span></strong> </td>
+                                <td style="color: #fff"><strong><span class="amount">R$ <?php echo htmlspecialchars( $cart["vltotal"], ENT_COMPAT, 'UTF-8', FALSE ); ?></span></strong> </td>
                             </tr>
                         </tfoot>
                     </table>
